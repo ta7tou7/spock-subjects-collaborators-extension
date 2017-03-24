1f162eb596684352a714ce51b6b555597fffbc94
Mohamed HAMDI <mohamed.hamdi@ullink.com>
2017-03-24 17:48:36 +0100

    inject initialized fields in some cases
    Change-Id: I081f8e46a99a76ce26dff4d9e4c2d10698eabfa3

--- src/main/groovy/com/blogspot/toomuchcoding/spock/subjcollabs/FieldRetriever.groovy
+++ src/main/groovy/com/blogspot/toomuchcoding/spock/subjcollabs/FieldRetriever.groovy
@@ -5,22 +5,23 @@ import org.codehaus.groovy.reflection.ClassInfo
 import org.spockframework.runtime.model.FieldInfo
 
 import java.lang.reflect.Field
+import java.lang.reflect.Modifier
+import static java.lang.reflect.Modifier.isFinal
 
 @PackageScope
 class FieldRetriever {
 
 	Map<Field, Field> getAllUnsetFields(Collection<Field> injectionCandidates, FieldInfo fieldInfo, Object subject) {
-		Map<Field, Field> fields = getAllMatchingFields(injectionCandidates, fieldInfo)
-		return fields.findAll { Field field, Field injectionCandidate -> field.get(subject) == null }
+		return getAllMatchingFields(injectionCandidates, fieldInfo, subject)
 	}
 
-	private Map<Field, Field> getAllMatchingFields(Collection<Field> injectionCandidates, FieldInfo fieldInfo) {
+	private Map<Field, Field> getAllMatchingFields(Collection<Field> injectionCandidates, FieldInfo fieldInfo, Object subject) {
 		List<Field> fields = getAllFieldsFromSubject(fieldInfo.type, [])
-		return getMatchingFieldsBasingOnTypeAndPropertyName(injectionCandidates, fields)
+		return getMatchingFieldsBasingOnTypeAndPropertyName(injectionCandidates, fields, subject)
 	}
 
 	private List<Field> getAllFieldsFromSubject(Class type, List<Field> fields) {
-		fields.addAll(type.declaredFields.findAll { Field field -> !field.isSynthetic() && field.type != ClassInfo })
+		fields.addAll(type.declaredFields.findAll { Field field -> !field.isSynthetic() && !isFinal(field.getModifiers()) && field.type != ClassInfo })
 		if (type.superclass != null) {
 			fields.addAll(getAllFieldsFromSubject(type.superclass, fields))
 		}
@@ -28,14 +29,24 @@ class FieldRetriever {
 		return fields
 	}
 
-	private Map getMatchingFieldsBasingOnTypeAndPropertyName(Collection<Field> injectionCandidates, List<Field> allFields) {
+	private Map getMatchingFieldsBasingOnTypeAndPropertyName(Collection<Field> injectionCandidates, List<Field> allFields, Object subject) {
 		Map matchingFields = [:]
 		injectionCandidates.each { Field injectionCandidate ->
 			List<Field> matchingTypes = allFields.findAll { it.type == injectionCandidate.type }
 			Field injectionCandidateByNameAndType = matchingTypes.find { it.name.equalsIgnoreCase(injectionCandidate.name) }
 			if (injectionCandidateByNameAndType) {
 				matchingFields[injectionCandidateByNameAndType] = injectionCandidate
-			} else {
+			}
+            else if(matchingTypes.size() > 1) {
+                List<Field> nonUnSetMatchingTypes = matchingTypes.findAll { it.get(subject) == null }  
+                if(nonUnSetMatchingTypes.isEmpty()) {
+                    matchingFields = matchingFields << matchingTypes.collectEntries { [(it) : injectionCandidate] }
+                }
+                else {
+                    matchingFields = matchingFields << nonUnSetMatchingTypes.collectEntries { [(it) : injectionCandidate] }
+                }
+            }
+             else {
 				matchingFields = matchingFields << matchingTypes.collectEntries { [(it) : injectionCandidate] }
 			}
 		}
